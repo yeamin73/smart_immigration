3b34a0f99c87e7cd72d2ae34e802c40780a60b7e
y2-ripty <yeaminrahman23@gmail.com>
2025-10-02 13:56:31 +0100

    Day one just starting a new project

---
+++ start.py
@@ -0,0 +1,7 @@
+# This script prints the string "hello immigration" to the console.
+def greet_immigration():
+    """Prints a greeting message."""
+    print("hello immigration")
+
+# Execute the function
+greet_immigration()
